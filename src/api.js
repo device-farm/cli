44ce33820451435c4d8e484ef0d1de543ad74459
Pavel Burgr <pb@drake.cz>
2021-05-25 00:26:17 +0200

    WIP

---
+++ src/api.js
@@ -0,0 +1,84 @@
+const io = require("socket.io-client").io;
+
+module.exports = async ({ getKey, url }) => {
+
+    function getApi() {
+
+        return new Promise((resolve, reject) => {
+
+            const socket = io(url, {
+                transports: ["polling", "websocket"]
+            });
+
+            socket.on("event", (apiName, eventName, args) => {
+                console.info("EVENT!", ...this.arguments);
+            });
+
+            socket.on("connect_error", e => {
+                reject(e);
+            });
+
+            socket.on("connect", () => {
+
+                socket.emit("discover", "1.0", info => {
+
+                    let api = {
+                        close() {
+                            socket.close();
+                        }
+                    };
+
+                    for (let apiName in info.api) {
+                        api[apiName] = {};
+                        for (let fncName in info.api[apiName]) {
+                            api[apiName][fncName] = async function (...args) {
+                                return new Promise((resolveCall, rejectCall) => {
+                                    socket.emit("call", {
+                                        api: apiName,
+                                        fnc: fncName,
+                                        args
+                                    }, reply => {
+                                        if (reply.error) {
+                                            rejectCall(reply.error);
+                                        } else {
+                                            resolveCall(reply.result);
+                                        }
+                                    });
+                                });
+
+                            };
+                        }
+                    }
+
+                    // Object.entries(info.events).forEach(([apiName, events]) => {
+                    //     events.forEach(eventName => {
+                    //         let initcap = s => s.charAt(0).toUpperCase() + s.slice(1);
+                    //         let methodName = "on" + (apiName ? initcap(apiName) : "") + initcap(eventName);
+                    //         let jqName = "webglue." + (apiName ? apiName + "." : "") + eventName;
+                    //         $.fn[methodName] = function (handler) {
+                    //             this.on(jqName, (e, ...args) => {
+                    //                 if (e.currentTarget === e.target) {
+                    //                     handler.apply(handler, args);
+                    //                 }
+                    //             });
+                    //             return this;
+                    //         };
+                    //     });
+                    // });
+
+                    resolve(api);
+                });
+
+            });
+
+        });
+
+    }
+
+    return async () => {
+        let apiKey = getKey();
+        let api = await getApi();
+        api.authorization = await api.portal.authorizeSession({ apiKey });
+        return api;
+    };
+}
--- src/install.js
+++ src/install.js
@@ -1,11 +1,119 @@
+const { spawn } = require("child_process");
+const fs = require("fs").promises;
 
-module.exports = async ({ user }) => {
+module.exports = async ({ api: createApi }) => {
+
+    function exec(command, args, env) {
+        return new Promise((resolve, reject) => {
+
+            let proc = spawn(command, args, {
+                env: { ...process.env, ...env },
+                stdio: ["inherit", "inherit", "inherit"]
+            });
+
+            proc.on("exit", (code, signal) => {
+                resolve({ code, signal });
+            });
+
+            proc.on("error", error => {
+                reject(error);
+            });
+        });
+    }
+
+    function base64(str) {
+        return Buffer.from(str).toString("base64");
+    }
+
+    async function install(deviceId, blkDevice, {
+        rootOverlayDir,
+        dtoDir,
+        wifi,
+        sshPubKeysFile
+    }, log, dryRun) {
+
+        let api = await createApi();
+        try {
+
+            let { portal } = api;
+
+            let detail = await portal.getDeviceDetail({ deviceId });
+            if (!detail.device.board) {
+                throw new Error("No board information for this device.");
+            }
+
+            let wireguardSettings = await portal.getWireguardSettings({ deviceId });
+
+            let environment = {
+                INSTALL_DEVICE: blkDevice,
+                CONFIG_HOSTNAME: detail.device.id,
+                CONFIG_WG_DEVICE_ADDR: wireguardSettings.address,
+                CONFIG_WG_DEVICE_MASK: wireguardSettings.cidr,
+                CONFIG_WG_SERVER_PUBLIC_KEY: wireguardSettings.server.publicKey,
+                CONFIG_WG_SERVER_HOST: wireguardSettings.server.host,
+                CONFIG_WG_SERVER_PORT: wireguardSettings.server.port,               
+                ...wifi ? {
+                    CONFIG_WIFI_SSID: base64(wifi.ssid),
+                    CONFIG_WIFI_PASSWORD: base64(wifi.password)
+                } : {},
+                ...sshPubKeysFile ? {
+                    CONFIG_SSH_AUTHORIZED_KEYS: base64(await fs.readFile(sshPubKeysFile))
+                } : {}
+            };
+
+            let command = [
+                `docker`, `run`,
+                `--rm`,
+                `--volume=/dev:/build/host/dev`,
+                `--volume=/tmp/${deviceId}:/build/output`,
+                ...rootOverlayDir ? [
+                    `--volume=${rootOverlayDir}:/build/custom-root`
+                ] : [],
+                ...dtoDir ? [
+                    `--volume=${dtoDir}:/build/custom-dto`
+                ] : [],
+                ...Object.entries(environment)
+                    .map(([k, v]) => `--env=${k}=${v}`),
+                `--privileged`,
+                detail.device.board.installer
+            ];
+
+
+            if (dryRun) {
+
+                log(command.join(" "));
+
+            } else {
+
+                await exec(command.shift(), command);
+                let wireguardUpdate = JSON.parse(await fs.readFile(`/tmp/${deviceId}/wg.json`));
+                await portal.setWireguardKeys({
+                    deviceId,
+                    publicKey: wireguardUpdate.publicKey,
+                    presharedKey: wireguardUpdate.presharedKey
+                }); 
+
+            }
+
+        } finally {
+            api.close();
+        }
+    }
+
+    function parseWifi(str) {
+        let match = /(?<ssid>[^:]*):(?<password>.*)/.exec(str);
+        if (!match) {
+            throw new Error("Error parsing WiFi parameters.");
+        }
+        return match.groups;
+    }
 
     return {
         name: "install <device-id> <blk-device>",
         description: "installs DEVICE.FARM Linux on given block device",
         define(program) {
             program
+                .option("-D, --dry", "dry run - show docker command line only")
                 .option("-w, --wifi <ssid:password>", "pre-configures WiFi connection")
                 .option("-s, --ssh <pub-key-file>", "installs public SSH key for user root\nuse - as shorthand for ~/.ssh/id_rsa.pub")
                 .option("-r, --root <overlay-dir>", "root overlay directory")
@@ -16,12 +124,19 @@ module.exports = async ({ user }) => {
                 });
         },
 
-        async run(deviceId, blkDevice, { wifi, ssh, root, dto }) {
+        async run(deviceId, blkDevice, { wifi, ssh, root, dto, dry }) {
 
-            let apiKey = user.getApiKey();
-            
-            console.info(apiKey);
-            console.info(deviceId, blkDevice, wifi, ssh, root, dto);
+            await install(deviceId, blkDevice, {
+                wifi: wifi && parseWifi(wifi),
+                sshPubKeysFile: ssh && (ssh === "-" ? `${process.env.HOME}/.ssh/id_rsa.pub` : ssh),
+                rootOverlayDir: root,
+                dtoDir: dto
+            }, async line => {
+                console.info(line);
+            }, dry);
+
+            console.info("Done.");
+
         }
     }
 }
