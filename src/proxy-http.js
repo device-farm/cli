5c1059fc6e337db3da2579fbb7e8f61b16519e6e
Pavel Burgr <pb@drake.cz>
2023-12-22 17:12:23 +0100

    Fix test

--- src/proxy-http.js
+++ src/proxy-http.js
@@ -36,7 +36,6 @@ module.exports = async ({ }) => function ({ apiKey, port, deviceId, service }) {
 
                 let targetReq = http.request(target.url, target.options, targetRes => {
                     res.writeHead(targetRes.statusCode, targetRes.statusMessage, targetRes.headers);
-                    res.write("");
                     targetRes.pipe(res);
                 });
 
